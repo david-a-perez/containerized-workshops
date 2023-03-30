c9aa345f6f7d48f03931dacab2cc8e7a18c6354d
Rkhurana19 <khuranarohit75@gmail.com>
2023-03-30 16:27:10 -0500

    connect docker to cloud

--- backend/ContainerizedWorkshops/views.py
+++ backend/ContainerizedWorkshops/views.py
@@ -11,6 +11,8 @@ import shortuuid
 from .serializers import WorkshopSerializer, ParticipantSerializer, ContainerSerializer
 from .models import Workshop, Participant
 
+import paramiko
+
 # TODO: reminder to use get_serializer_class to have seperate read and write serializers
 # TODO: reminder to use permissions
 # TODO: reminder to add filterset_fields to other views
@@ -57,31 +59,30 @@ class ContainerViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def list(self, request):
-        # client = docker.from_env()
-        # containers: "list[Container]" = client.containers.list(all=True, filters={
-        #     "label": [f"{Labels.user_id.value}={request.user.id}"]})  # type: ignore
-        # serializer = ContainerSerializer([serialize_container(
-        #     container) for container in containers], many=True)
-        # return Response(serializer.data)
-
-        return Response("[1]")
+        client = docker.from_env()
+        containers: "list[Container]" = client.containers.list(all=True, filters={
+            "label": [f"{Labels.user_id.value}={request.user.id}"]})  # type: ignore
+        serializer = ContainerSerializer([serialize_container(
+            container) for container in containers], many=True)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         if not pk:
             raise APIException()
-        client = docker.from_env()
+        client = docker.from_env() # TODO: replace with DockerClient
         container: Container = client.containers.get(pk)  # type: ignore
         serializer = ContainerSerializer(serialize_container(container))
         return Response(serializer.data)
 
     def create(self, request):
+        # TODO: verify participant does not already have container running
         if not request.data['participant']:
             raise APIException()
         participant = Participant.objects.filter(
             pk=request.data['participant']).first()
         if not participant:
             raise NotInWorkshop()
-        client = docker.from_env()
+        client = docker.DockerClient(base_url = "ssh://cc@cham-worker2") # TODO: from environment variable (implement basic scheduling algorithm)
         container: Container = client.containers.run(
             participant.workshop.docker_tag, auto_remove=True, detach=True,
             environment={"SSH_PUBLIC_KEY": "ABCD"},
