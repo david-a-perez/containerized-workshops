1a89d265426a83ef559f3ee30c58c043e91878c6
David Perez <david.a.perez@vanderbilt.edu>
2023-04-08 18:57:04 -0500

    Refactor Container ViewSet

---
+++ backend/ContainerizedWorkshops/container.py
@@ -0,0 +1,208 @@
+from enum import Enum, unique
+import functools
+import secrets
+import socket
+from docker import DockerClient
+from docker.models.containers import Container
+import docker.transport
+import docker.errors
+import paramiko.ssh_exception
+
+from ContainerizedWorkshops.models import Workshop
+
+from backend.settings.base import CONTROLLER_ID, DOCKER_HOSTS
+
+
+class CachedDockerClient(DockerClient):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    @functools.cached_property
+    def api(self):
+        return docker.APIClient(*self.args, **self.kwargs)
+
+    @property
+    @functools.wraps(DockerClient.configs.getter)
+    def configs(self):
+        try:
+            return super().configs
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().configs
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().configs
+
+    @property
+    @functools.wraps(DockerClient.containers.getter)
+    def containers(self):
+        try:
+            return super().containers
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().containers
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().containers
+
+    @property
+    @functools.wraps(DockerClient.images.getter)
+    def images(self):
+        try:
+            return super().images
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().images
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().images
+
+    @property
+    @functools.wraps(DockerClient.networks.getter)
+    def networks(self):
+        try:
+            return super().networks
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().networks
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().networks
+
+    @property
+    @functools.wraps(DockerClient.nodes.getter)
+    def nodes(self):
+        try:
+            return super().nodes
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().nodes
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().nodes
+
+    @property
+    @functools.wraps(DockerClient.plugins.getter)
+    def plugins(self):
+        try:
+            return super().plugins
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().plugins
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().plugins
+
+    @property
+    @functools.wraps(DockerClient.secrets.getter)
+    def secrets(self):
+        try:
+            return super().secrets
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().secrets
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().secrets
+
+    @property
+    @functools.wraps(DockerClient.services.getter)
+    def services(self):
+        try:
+            return super().services
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().services
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().services
+
+    @property
+    @functools.wraps(DockerClient.swarm.getter)
+    def swarm(self):
+        try:
+            return super().swarm
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().swarm
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().swarm
+
+    @property
+    @functools.wraps(DockerClient.volumes.getter)
+    def volumes(self):
+        try:
+            return super().volumes
+        except socket.error as e:
+            print("Ignoring error:", e)
+            return super().volumes
+        except paramiko.ssh_exception.SSHException as e:
+            print("Ignoring error:", e)
+            return super().volumes
+
+
+DOCKER_CLIENTS = [CachedDockerClient(base_url=base_url)
+                  for base_url in DOCKER_HOSTS]
+
+
+@unique
+class Labels(str, Enum):
+    workshop_id = 'com.containerized-workshops.workshop-id'
+    user_id = 'com.containerized-workshops.user-id'
+    controller_id = 'com.containerized-workshops.controller-id'
+
+
+def list_containers(workshop_id: str | None, user_id: str | None) -> list[Container]:
+    workshop_id_label = f"{Labels.workshop_id.value}={workshop_id}" if workshop_id else Labels.workshop_id.value
+    user_id_label = f"{Labels.user_id.value}={user_id}" if user_id else Labels.user_id.value
+    controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
+
+    return [container  # type: ignore
+            for client in DOCKER_CLIENTS
+            for container in client.containers.list(all=True, filters={
+                "label": [workshop_id_label, user_id_label, controller_id_label]})]
+
+
+def create_container(workshop: Workshop, user_id: str, public_key: str) -> Container:
+    workshop_id_label = f"{Labels.workshop_id.value}={workshop.pk}"
+    user_id_label = f"{Labels.user_id.value}={user_id}"
+    controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
+
+    for client in DOCKER_CLIENTS:
+        containers = client.containers.list(all=True, filters={
+            "label": [workshop_id_label, user_id_label, controller_id_label]})
+        if containers:
+            return containers[0]  # type: ignore
+
+    def get_workload(client: DockerClient):
+        info = client.info()
+        return info['ContainersRunning'] / info['NCPU']
+
+    client = min(DOCKER_CLIENTS, key=get_workload)
+
+    # TODO: make docker images cloud based
+
+    container = client.containers.run(
+        workshop.docker_tag, auto_remove=True, detach=True,
+        environment={"SSH_PUBLIC_KEY": public_key,
+                     "JUPYTER_TOKEN": secrets.token_hex(24)},
+        publish_all_ports=True,
+        labels={Labels.workshop_id.value: str(workshop.pk),
+                Labels.user_id.value: str(user_id),
+                Labels.controller_id.value: CONTROLLER_ID},
+        cpu_period=100000,
+        cpu_quota=50000,
+        mem_limit="1g")
+    return container  # type: ignore
+
+
+def clear_containers(workshop_id: str | None, user_id: str | None):
+    workshop_id_label = f"{Labels.workshop_id.value}={workshop_id}" if workshop_id else Labels.workshop_id.value
+    user_id_label = f"{Labels.user_id.value}={user_id}" if user_id else Labels.user_id.value
+    controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
+
+    for client in DOCKER_CLIENTS:
+        for container in client.containers.list(all=True, filters={"label": [workshop_id_label, user_id_label, controller_id_label]}):
+            container.stop()  # type: ignore
--- backend/ContainerizedWorkshops/views.py
+++ backend/ContainerizedWorkshops/views.py
@@ -1,21 +1,15 @@
-from enum import Enum, unique
-from functools import cached_property
-import functools
-import socket
-import paramiko.ssh_exception
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.exceptions import APIException, PermissionDenied, NotFound
+from rest_framework.exceptions import APIException
 
 import docker
 import docker.errors
 import docker.transport
 from docker.models.containers import Container
 from docker import DockerClient
-import secrets
+from ContainerizedWorkshops.container import Labels, clear_containers, create_container, list_containers
 
-from backend.settings.base import CONTROLLER_ID, DOCKER_HOSTS
 from .serializers import WorkshopSerializer, WorkshopReadSerializer, SnippetSerializer, TunneledPortSerializer, ContainerSerializer, UserSerializer
 from .models import Workshop, Snippet, TunneledPort
 from django.contrib.auth.models import User
@@ -99,22 +93,21 @@ class NotAdminOrParticipant(APIException):
     default_code = "not_admin_or_participant"
 
 
-@unique
-class Labels(str, Enum):
-    workshop_id = 'com.containerized-workshops.workshop-id'
-    user_id = 'com.containerized-workshops.user-id'
-    controller_id = 'com.containerized-workshops.controller-id'
-
-
-def serialize_container(client: DockerClient, container: Container):
+def serialize_container(container: Container):
+    if not container.client:
+        raise APIException()
+    if not container.attrs:
+        raise APIException()
+    
+    client: DockerClient = container.client
     public_ip = client.api._custom_adapter.ssh_params['hostname'] if isinstance(
         client.api._custom_adapter, docker.transport.SSHHTTPAdapter) else "127.0.0.1"
 
-    ports = container.attrs["NetworkSettings"]["Ports"]  # type: ignore
+    ports = container.attrs["NetworkSettings"]["Ports"]
     ports = [{"protocol": port.split("/")[1], "container_port": port.split("/")[0], "host_port": host_ports[0]
               ['HostPort'] if host_ports and len(host_ports) > 0 else None} for port, host_ports in ports.items()]
 
-    env_vars = container.attrs["Config"]["Env"]  # type: ignore
+    env_vars = container.attrs["Config"]["Env"]
 
     return {"id": container.id,
             "workshop_id": Workshop.objects.get(pk=container.labels[Labels.workshop_id.value]),
@@ -126,180 +119,18 @@ def serialize_container(client: DockerClient, container: Container):
             "jupyter_token": next(env.split("=", 1)[1] for env in env_vars if env.startswith("JUPYTER_TOKEN="))}
 
 
-
-class CachedDockerClient(DockerClient):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-    @cached_property
-    def api(self):
-        return docker.APIClient(*self.args, **self.kwargs)
-    
-    @property
-    @functools.wraps(DockerClient.configs.getter)
-    def configs(self):
-        try:
-            return super().configs
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().configs
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().configs
-
-    @property
-    @functools.wraps(DockerClient.containers.getter)
-    def containers(self):
-        try:
-            return super().containers
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().containers
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().containers
-
-    @property
-    @functools.wraps(DockerClient.images.getter)
-    def images(self):
-        try:
-            return super().images
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().images
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().images
-
-    @property
-    @functools.wraps(DockerClient.networks.getter)
-    def networks(self):
-        try:
-            return super().networks
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().networks
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().networks
-
-    @property
-    @functools.wraps(DockerClient.nodes.getter)
-    def nodes(self):
-        try:
-            return super().nodes
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().nodes
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().nodes
-
-    @property
-    @functools.wraps(DockerClient.plugins.getter)
-    def plugins(self):
-        try:
-            return super().plugins
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().plugins
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().plugins
-
-    @property
-    @functools.wraps(DockerClient.secrets.getter)
-    def secrets(self):
-        try:
-            return super().secrets
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().secrets
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().secrets
-
-    @property
-    @functools.wraps(DockerClient.services.getter)
-    def services(self):
-        try:
-            return super().services
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().services
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().services
-
-    @property
-    @functools.wraps(DockerClient.swarm.getter)
-    def swarm(self):
-        try:
-            return super().swarm
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().swarm
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().swarm
-
-    @property
-    @functools.wraps(DockerClient.volumes.getter)
-    def volumes(self):
-        try:
-            return super().volumes
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().volumes
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().volumes
-
-
-DOCKER_CLIENTS = [CachedDockerClient(base_url=base_url) for base_url in DOCKER_HOSTS]
-
-
 class ContainerViewSet(viewsets.ViewSet):
     serializer_class = ContainerSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def list(self, request):
-        workshop_id_label = f"{Labels.workshop_id.value}={request.query_params['workshop_id']}" if "workshop_id" in request.query_params else Labels.workshop_id.value
-        controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
+        containers = list_containers(
+            request.query_params['workshop_id'] if 'workshop_id' in request.query_params else None,
+            (request.query_params['user_id'] if 'user_id' in request.query_params else None) if request.user.is_superuser else str(request.user.pk))
 
-        if request.user.is_superuser:
-            user_id_label = f"{Labels.user_id.value}={request.query_params['user_id']}" if "user_id" in request.query_params else Labels.user_id.value
-        else:
-            user_id_label = f"{Labels.user_id.value}={request.user.pk}"
-
-        def get_containers(client: DockerClient):
-            containers: "list[Container]" = client.containers.list(all=True, filters={
-                "label": [workshop_id_label, user_id_label, controller_id_label]})  # type: ignore
-
-            return [serialize_container(client, container) for container in containers]
-
-        serializer = ContainerSerializer(
-            [serialized_container for client in DOCKER_CLIENTS for serialized_container in get_containers(client)], many=True)
+        serializer = ContainerSerializer([serialize_container(container) for container in containers], many=True)
 
         return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        if not pk:
-            raise APIException()
-
-        for client in DOCKER_CLIENTS:
-            try:
-                container: Container = client.containers.get(
-                    pk)  # type: ignore
-            except docker.errors.NotFound:
-                continue
-            if not (request.user.is_superuser or container.labels[Labels.user_id.value] == str(request.user.pk)):
-                raise PermissionDenied()
-            serializer = ContainerSerializer(
-                serialize_container(client, container))
-            return Response(serializer.data)
-        return NotFound()
 
     def create(self, request):
         if not request.data['workshop_id']:
@@ -319,62 +150,20 @@ class ContainerViewSet(viewsets.ViewSet):
 
         if not workshop.participants.filter(pk=request.data['user_id']).exists():
             raise NotInWorkshop()
+        
+        container = create_container(workshop, request.data['user_id'], request.data['public_key'])
 
-        workshop_id_label = f"{Labels.workshop_id.value}={request.data['workshop_id']}"
-        user_id_label = f"{Labels.user_id.value}={request.data['user_id']}"
-        controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
-
-        for client in DOCKER_CLIENTS:
-            containers: "list[Container]" = client.containers.list(all=True, filters={
-                "label": [workshop_id_label, user_id_label, controller_id_label]})  # type: ignore
-            if containers:
-                serializer = ContainerSerializer(
-                    serialize_container(client, containers[0]))
-                return Response(serializer.data)
-
-        def get_workload(client: DockerClient):
-            info = client.info()
-            return info['ContainersRunning'] / info['NCPU']
-
-        for client in DOCKER_CLIENTS:
-            info = client.info()
-            print(info['ContainersRunning'])
-
-        client = min(DOCKER_CLIENTS, key=get_workload)
-
-        # TODO: make docker images cloud based
-
-        container: Container = client.containers.run(
-            workshop.docker_tag, auto_remove=True, detach=True,
-            environment={"SSH_PUBLIC_KEY": request.data["public_key"],
-                         "JUPYTER_TOKEN": secrets.token_hex(24)},
-            publish_all_ports=True,
-            labels={Labels.workshop_id.value: str(workshop.pk),
-                    Labels.user_id.value: str(request.data['user_id']),
-                    Labels.controller_id.value: CONTROLLER_ID},
-            cpu_period=100000,
-            cpu_quota=50000,
-            mem_limit="1g")  # type: ignore
-        serializer = ContainerSerializer(
-            serialize_container(client, container))
+        serializer = ContainerSerializer(serialize_container(container))
         return Response(serializer.data)
 
-    def destroy(self, request, pk=None):
-        if not pk:
-            raise APIException()
-        for client in DOCKER_CLIENTS:
-            try:
-                container: Container = client.containers.get(
-                    pk)  # type: ignore
-            except docker.errors.NotFound:
-                continue
-            if not (request.user.is_superuser or container.labels[Labels.user_id.value] == str(request.user.pk)):
-                raise NotAdminOrParticipant()
-            container.stop()
-            serializer = ContainerSerializer(
-                serialize_container(client, container))
-            return Response(serializer.data)
-        return NotFound()
+    
+    @action(detail=False, methods=['post'])
+    def clear(self, request):
+        clear_containers(
+            request.query_params['workshop_id'] if 'workshop_id' in request.query_params else None,
+            (request.query_params['user_id'] if 'user_id' in request.query_params else None) if request.user.is_superuser else str(request.user.pk))
+        
+        return Response()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
