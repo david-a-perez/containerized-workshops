cbd29b6d5bf3d042a6273be8b0206f4d136a8158
David Perez <david.a.perez@vanderbilt.edu>
2023-04-12 00:48:55 -0500

    Remove CachedDockerClient and properly implement retry

--- backend/ContainerizedWorkshops/container.py
+++ backend/ContainerizedWorkshops/container.py
@@ -1,11 +1,8 @@
 from enum import Enum, unique
-import functools
 import secrets
 import socket
 from docker import DockerClient
 from docker.models.containers import Container
-import docker.transport
-import docker.errors
 import paramiko.ssh_exception
 
 from ContainerizedWorkshops.models import Workshop
@@ -13,139 +10,18 @@ from ContainerizedWorkshops.models import Workshop
 from backend.settings.base import CONTROLLER_ID, DOCKER_HOSTS
 
 
-class CachedDockerClient(DockerClient):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-    @functools.cached_property
-    def api(self):
-        return docker.APIClient(*self.args, **self.kwargs)
-
-    @property
-    @functools.wraps(DockerClient.configs.getter)
-    def configs(self):
-        try:
-            return super().configs
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().configs
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().configs
-
-    @property
-    @functools.wraps(DockerClient.containers.getter)
-    def containers(self):
-        try:
-            return super().containers
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().containers
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().containers
-
-    @property
-    @functools.wraps(DockerClient.images.getter)
-    def images(self):
-        try:
-            return super().images
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().images
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().images
-
-    @property
-    @functools.wraps(DockerClient.networks.getter)
-    def networks(self):
-        try:
-            return super().networks
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().networks
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().networks
-
-    @property
-    @functools.wraps(DockerClient.nodes.getter)
-    def nodes(self):
-        try:
-            return super().nodes
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().nodes
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().nodes
-
-    @property
-    @functools.wraps(DockerClient.plugins.getter)
-    def plugins(self):
-        try:
-            return super().plugins
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().plugins
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().plugins
-
-    @property
-    @functools.wraps(DockerClient.secrets.getter)
-    def secrets(self):
-        try:
-            return super().secrets
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().secrets
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().secrets
-
-    @property
-    @functools.wraps(DockerClient.services.getter)
-    def services(self):
-        try:
-            return super().services
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().services
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().services
-
-    @property
-    @functools.wraps(DockerClient.swarm.getter)
-    def swarm(self):
-        try:
-            return super().swarm
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().swarm
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().swarm
-
-    @property
-    @functools.wraps(DockerClient.volumes.getter)
-    def volumes(self):
-        try:
-            return super().volumes
-        except socket.error as e:
-            print("Ignoring error:", e)
-            return super().volumes
-        except paramiko.ssh_exception.SSHException as e:
-            print("Ignoring error:", e)
-            return super().volumes
-
-
-DOCKER_CLIENTS = [CachedDockerClient(base_url=base_url)
+DOCKER_CLIENTS = [DockerClient(base_url=base_url)
                   for base_url in DOCKER_HOSTS]
 
+def redo_on_connection_error(x):
+    try:
+        return x()
+    except socket.error as e:
+        print("Socket error ignored:", e)
+        return x()
+    except paramiko.ssh_exception.SSHException as e:
+        print("Socket error ignored:", e)
+        return x()
 
 @unique
 class Labels(str, Enum):
@@ -161,8 +37,8 @@ def list_containers(workshop_id: str | None, user_id: str | None) -> list[Contai
 
     return [container  # type: ignore
             for client in DOCKER_CLIENTS
-            for container in client.containers.list(all=True, filters={
-                "label": [workshop_id_label, user_id_label, controller_id_label]})]
+            for container in redo_on_connection_error(lambda: client.containers.list(all=True, filters={
+                "label": [workshop_id_label, user_id_label, controller_id_label]}))]
 
 
 def create_container(workshop: Workshop, user_id: str, public_key: str) -> Container:
@@ -171,20 +47,23 @@ def create_container(workshop: Workshop, user_id: str, public_key: str) -> Conta
     controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
 
     for client in DOCKER_CLIENTS:
-        containers = client.containers.list(all=True, filters={
-            "label": [workshop_id_label, user_id_label, controller_id_label]})
+        containers = redo_on_connection_error(lambda: client.containers.list(all=True, filters={
+            "label": [workshop_id_label, user_id_label, controller_id_label]}))
         if containers:
             return containers[0]  # type: ignore
 
     def get_workload(client: DockerClient):
-        info = client.info()
+        info = redo_on_connection_error(lambda: client.info())
         return info['ContainersRunning'] / info['NCPU']
 
     client = min(DOCKER_CLIENTS, key=get_workload)
 
-    # TODO: make docker images cloud based
+    split_docker_tag = workshop.docker_tag.split(":")
 
-    container = client.containers.run(
+    redo_on_connection_error(lambda: client.images.pull(split_docker_tag[0], split_docker_tag[1] if len(
+        split_docker_tag) > 1 else None))
+
+    container = redo_on_connection_error(lambda: client.containers.run(
         workshop.docker_tag, auto_remove=True, detach=True,
         environment={"SSH_PUBLIC_KEY": public_key,
                      "JUPYTER_TOKEN": secrets.token_hex(24)},
@@ -194,7 +73,7 @@ def create_container(workshop: Workshop, user_id: str, public_key: str) -> Conta
                 Labels.controller_id.value: CONTROLLER_ID},
         cpu_period=100000,
         cpu_quota=50000,
-        mem_limit="1g")
+        mem_limit="1g"))
     return container  # type: ignore
 
 
@@ -204,5 +83,5 @@ def clear_containers(workshop_id: str | None, user_id: str | None):
     controller_id_label = f"{Labels.controller_id.value}={CONTROLLER_ID}"
 
     for client in DOCKER_CLIENTS:
-        for container in client.containers.list(all=True, filters={"label": [workshop_id_label, user_id_label, controller_id_label]}):
+        for container in redo_on_connection_error(lambda: client.containers.list(all=True, filters={"label": [workshop_id_label, user_id_label, controller_id_label]})):
             container.stop()  # type: ignore
