d9f3d596ed44252bb3bf19c32438b2dc45f417f2
David Perez <david.a.perez@vanderbilt.edu>
2023-04-20 14:03:34 -0500

    Fix typo

--- backend/ContainerizedWorkshops/views.py
+++ backend/ContainerizedWorkshops/views.py
@@ -12,7 +12,7 @@ import docker
 from rest_framework.exceptions import APIException
 from rest_framework.decorators import action
 from rest_framework.response import Response
-git from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
